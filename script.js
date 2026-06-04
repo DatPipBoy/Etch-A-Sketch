9b295ac66ffab781e0be08d456b7933d34542695
Eric <116214829+DatPipBoy@users.noreply.github.com>
2026-06-04 12:16:45 -0400

    Fixed syntax

--- script.js
+++ script.js
@@ -96,5 +96,5 @@ function randomColor() {
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
 
-    return `rgb(${r},${g},${b}`;
+    return `rgb(${r},${g},${b})`;
 }
